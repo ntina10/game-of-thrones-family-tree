9c2771103f44822794af767f24c91ffb3018b55f
ntina10 <frenadin@hotmail.com>
2025-09-13 15:54:33 +0300

    added characters title and edges

--- src/components/CharacterNode.jsx
+++ src/components/CharacterNode.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { Handle, Position } from "reactflow";
 import "./CharacterNode.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCrown,
+  faHand,
+  faHandshake,
+  faFaceAngry,
+} from "@fortawesome/free-solid-svg-icons";
+
+const titleInfo = {
+  King: { icon: faCrown, color: "#FFD700" }, // A gold color for the king
+  "Hand of the King": { icon: faHand, color: "#f5f0adff" }, // A silver color
+  Helper: { icon: faHandshake, color: "#ddbff7ff" }, // An olive green color
+};
+
+const opinionInfo = {
+  Evil: { icon: faFaceAngry, color: "#8800ffff" }, // A red color for evil
+};
 
 // The data prop is passed automatically by React Flow
 function CharacterNode({ data }) {
   // We can create a dynamic class string
   const nodeClasses = `character-node ${data.house || ""}`;
+
+  const infoToRender = data.title ? titleInfo[data.title] : null;
+  const infoToRen = data.opinion ? opinionInfo[data.opinion] : null;
 
   return (
     // The main container for our custom node
@@ -22,6 +42,27 @@ function CharacterNode({ data }) {
 
       <div className="character-image-container">
         <img src={data.image} alt={data.name} className="character-image" />
+
+        <div className="character-info">
+          {infoToRender && (
+            <div className={`character-info-item ${data.title}`}>
+              <FontAwesomeIcon
+                icon={infoToRender.icon}
+                size="xl"
+                style={{ color: infoToRender.color }}
+              />
+            </div>
+          )}
+          {infoToRen && (
+            <div className={`character-info-item ${data.opinion}`}>
+              <FontAwesomeIcon
+                icon={infoToRen.icon}
+                size="xl"
+                style={{ color: infoToRen.color }}
+              />
+            </div>
+          )}
+        </div>
         {data.tag && (
           <div className={`status-tag ${data.tag.type}`}>{data.tag.text}</div>
         )}
--- src/components/HouseNode.jsx
+++ src/components/HouseNode.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import './HouseNode.css';
+import React from "react";
+import { Handle, Position } from "reactflow";
+import "./HouseNode.css";
 
-function HouseNode({data}) {
-    const name = data.label.toUpperCase();
-    return (
-        <div className='house-node'>
-            <img src={data.image} alt={data.label} />
-            <h2> {name} </h2>
-        </div>
-    );
+function HouseNode({ data }) {
+  const name = data.label.toUpperCase();
+  return (
+    <div className="house-node">
+      <Handle type="source" position={Position.Bottom} id="parent" />
+
+      <img src={data.image} alt={data.label} />
+      <h2> {name} </h2>
+    </div>
+  );
 }
 
 export default HouseNode;
